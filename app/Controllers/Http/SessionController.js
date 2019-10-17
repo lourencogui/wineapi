c586e476d4bdf93390f9d4137a1e69a9aaf5db8c
Guilherme Lourenco <guilherme.lourenco@agrotools.local>
2019-10-16 21:01:21 -0300

    retornando se o usuário é ou não admin na request de autenticação

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,13 @@
 'use strict'
-
+const User = use('App/Models/User')
 class SessionController {
   async store ({ request, response, auth }) {
     const { email, password } = request.all()
 
-    const token = auth.attempt(email, password)
+    const { token, type } = await auth.attempt(email, password)
+    const user = await User.findBy({ email })
 
-    return token
+    return { token, type, user }
   }
 }
 
