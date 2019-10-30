f3a7da5d2733ae9d5c95f087d37d18007b7b9376
Guilherme Lourenco <guilherme.lourenco@agrotools.local>
2019-10-30 20:42:22 -0300

    upload de arquivos no s3

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -1,30 +1,30 @@
 const crypto = require('crypto')
 const Helpers = use('Helpers')
 const Product = use('App/Models/Product')
-
+const Drive = use('Drive')
 const Database = use('Database')
 class ProductController {
   async store ({ request, response }) {
-    const data = JSON.parse(request.input('product'))
-    const avatar = request.file('avatar', {
-      types: ['image']
+    let data, url, result
+    request.multipart.field(async (name, value) => {
+      data = JSON.parse(value)
     })
 
-    const random = Helpers.promisify(crypto.randomBytes)
-    const fileName = await random(16)
-    await avatar.move(Helpers.publicPath('uploads'), {
-      name: `${fileName.toString('hex')}.png`,
-      overwrite: true
+    request.multipart.file('avatar', {}, async (f) => {
+      const random = Helpers.promisify(crypto.randomBytes)
+      const fileName = await random(16)
+      const fullName = `${fileName.toString('hex')}.png`
+
+      await Drive.disk('s3').put(fullName, f.stream)
+      url = await Drive.disk('s3').getUrl(fullName)
+
+      const trx = await Database.beginTransaction()
+      const product = await Product.create({ ...data, avatar: url }, trx)
+      await trx.commit()
+      result = product
     })
-
-    if (!avatar.moved()) {
-      return avatar.error()
-    }
-
-    const trx = await Database.beginTransaction()
-    const product = await Product.create({ ...data, avatar: `${Helpers.publicPath(`/uploads/${avatar.fileName}`)}` }, trx)
-    await trx.commit()
-    return product
+    await request.multipart.process()
+    return result
   }
 
   async index (req, res) {
