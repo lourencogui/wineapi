1f0409628c981c5e42449dcd7b1ea09c78ffa527
Guilherme Lourenco <guilherme.lourenco@agrotools.local>
2019-10-30 21:20:17 -0300

    .

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -11,13 +11,15 @@ class ProductController {
     })
 
     request.multipart.file('avatar', {}, async (f) => {
-      // const random = Helpers.promisify(crypto.randomBytes)
-      // const fileName = await random(16)
-      // const fullName = `${fileName.toString('hex')}.png`
+      const random = Helpers.promisify(crypto.randomBytes)
+      const fileName = await random(16)
+      const fullName = `${fileName.toString('hex')}.png`
 
-      // await Drive.disk('s3').put(fullName, f.stream)
-      // url = await Drive.disk('s3').getUrl(fullName)
-      url = 'teste'
+      await Drive.disk('s3').put(fullName, f.stream, {
+        ACL: 'public-read'
+      })
+      url = await Drive.disk('s3').getUrl(fullName)
+      // url = 'teste'
 
       const trx = await Database.beginTransaction()
       const product = await Product.create({ ...data, avatar: url }, trx)
